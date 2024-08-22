a82b04a44bcef3672fe8cc6c6fa1575459b6ebb0
nitishsingamsetti <nsingamsetti11@gmail.com>
2024-08-22 12:10:58 +0530

    index

--- app.py
+++ app.py
@@ -6,6 +6,16 @@ app=Flask(__name__)
 def home():
     return render_template('welcome.html')
 
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+app.route('/admincreate',methods=['GET','POST'])
+def admincreate():
+    return render_template('admincreate.html')
+
+
 app.run(debug=True,use_reloader=True)
+
 
 
